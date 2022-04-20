46c6ac77686f4b3085f8a4c31462550bb50a0f40
Eduard <dirol2016@list.ru>
2022-04-20 12:27:31 +0500

    2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = 5778;
+int numberB = 350;
+int numberC =5994;
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+Console.Write("max: ");
+Console.WriteLine(max);
