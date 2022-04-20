dd54d61e68177f0ed436639f782b16049b1b6327
Eduard <dirol2016@list.ru>
2022-04-20 12:26:52 +0500

    1

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA = 755;
+int numberB = 48883;
+if(numberA > numberB)
+{   
+    Console.Write("max: ");
+    Console.WriteLine(numberA);
+    Console.Write("min: ");
+    Console.WriteLine(numberB);
+}
+else 
+{       
+    Console.Write("min: ");
+    Console.WriteLine(numberA);
+    Console.Write("max: ");
+    Console.WriteLine(numberB);
+}
+
